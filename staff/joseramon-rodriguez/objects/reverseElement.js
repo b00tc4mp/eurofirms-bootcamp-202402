825a81ea59e81b8b8be694d13742992756beb7d5
jose ramon rodriguez <jrjosera1@gmail.com>
2024-02-28 10:48:41 +0100

    add file to objects #50

---
+++ staff/joseramon-rodriguez/objects/reverseElement.js
@@ -0,0 +1,32 @@
+function reverseElement(object) {
+    //1st copy the object in order to reverse it
+    var copy = { ...object }
+    //2nd use the copy to insert the properties in reversed order
+    for (var i = 0; i < object.length; i++) {
+        object[i] = copy[object.length - i - 1]
+    }
+
+    //3rd return the reversed object
+    return object
+}
+var cars = {
+    0: 'peugeot',
+    1: 'ford',
+    2: 'renault',
+    3: 'audi',
+    4: 'bmw',
+    5: 'mercedez',
+    6: 'bentley',
+    7: 'ferrari',
+    length: 8
+
+}
+
+console.log('CASE 1 : reverse cars and save it in cars2')
+
+var cars2 = reverseElement(cars)
+console.log(cars2)
+
+console.log('CASE 2: show cars ->now cars should be reversed')
+
+console.log(cars)
