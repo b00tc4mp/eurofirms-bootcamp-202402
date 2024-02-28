2acb0a30e2d1480cdbb121158d52f48b80bee60a
Ruben Sanchez <rubensala1980@gmail.com>
2024-02-28 19:23:04 +0100

    transcription of several archives #68

---
+++ staff/ruben-sanchez/liveshare/app/register.js
@@ -0,0 +1,44 @@
+function registerUser(name, birthdate , username, email, password) {
+
+    form.onsubmit = function (event) {
+        event.preventDefault()
+    }
+
+    var nameInput = form.querySelector('#name')
+    var name = nameInput.value
+
+    var birthdateInput = form.querySelector('#birthdate')
+    var birthdate = birthdateInput.value
+
+    var usernameInput = form.querySelector('#username')
+    var username = usernameInput.value
+
+    var emailInput = form.querySelector('#email')
+    var email = emailInput.value
+
+    var passowrdInput = form.querySelector('#password')
+    var password = passowrdInput.value
+
+    try {
+        registerUser(name, birthdate, username, email, password)
+
+        console.log('user registered')
+
+        alert('user registered')
+
+        form.reset()
+
+
+
+
+
+    } catch (error) {
+        console.error(error.message)
+
+        alert(error.message)
+
+    }
+
+
+
+}
