39adb37ec6360ad9488fe96c9944e771fc750a86
jose ramon rodriguez <jrjosera1@gmail.com>
2024-02-29 13:08:23 +0100

    add files to objects #50

---
+++ staff/joseramon-rodriguez/objects/sortElements2.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * function that will sort the properties of the object
+ * converted to string sorted by ascending order
+ * comparing their UTF-16 values
+ * 
+ * This function will return the original array sorted not a copy  
+ * 
+ * order->defined properties ->blank properties ->undefined properties 
+ */
+
+function sortElements2(object) {
+    //TODO
+}
+
