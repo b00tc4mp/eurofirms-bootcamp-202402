8d65cad50b7ab5948586a77876cacec2e323973f
Yeyusin <sergioocruiz@gmail.com>
2024-02-29 19:52:38 +0100

    upgraded join #79

--- staff/sergio-ocaña/objects/join.js
+++ staff/sergio-ocaña/objects/join.js
@@ -1,9 +1,22 @@
+function array(array, value) {
+    var addArray = ""
+    for (var i = 0; i < array.length; i++)
+        if (array[i] === 'object') {
+            array(array, value)
+        } else {
+            addArray = addArray + "'" + array[i] + "'" + value
+        }
+
+}
 function join(object, value) {
     var phrase = "'"
 
     if (value === undefined)
         value = ','
     for (var i = 0; i < object.length; i++) {
+        if (typeof (object[i] === "object"))
+            array(object[i], value)
+
         var string = object[i]
 
         if (i < object.length - 1)
@@ -29,4 +42,11 @@ var result = join(cities)
 console.log(result)
 
 var result = join(cities, '+')
+console.log(result)
+var arrCaballero = {
+    0: [0, 5, 32],
+    1: [1, [2, 22]],
+    length: 2
+}
+var result = join(arrCaballero)
 console.log(result)
