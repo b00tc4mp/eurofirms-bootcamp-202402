6d6571f79dc17c161670c669ac88828e7178b2fc
Manuel Barzi <manuelbarzi@gmail.com>
2024-02-29 17:51:44 +0100

    add login presentation and logic; separate login user logic from presentation #56

--- staff/manuel-barzi/liveshare/app.3/logic.js
+++ staff/manuel-barzi/liveshare/app.3/logic.js
@@ -84,35 +84,3 @@ function registerUser(name, birthdate, username, email, password) {
 
     localStorage.users = JSON.stringify(users)
 }
-
-function loginUser(username, password) {
-    if (username.length < 3)
-        throw new Error('username is lower than 3 characters')
-
-    if (username.includes(' '))
-        throw new Error('username has a space character')
-
-    if (password.length < 8)
-        throw new Error('password is lower than 8 characters')
-
-    if (password.includes(' '))
-        throw new Error('password has space character')
-
-    var user
-
-    for (var i = 0; i < users.length; i++) {
-        var user2 = users[i]
-
-        if (user2.username === username) {
-            user = user2
-
-            break
-        }
-    }
-
-    if (user === undefined)
-        throw new Error('user not found')
-
-    if (user.password !== password)
-        throw new Error('wrong password')
-}
