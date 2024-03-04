a51b4e1d9b584c1139f59bdfcf3f10b9eb0e19d3
Yeyusin <sergioocruiz@gmail.com>
2024-03-04 16:35:04 +0100

    WIP find #53

---
+++ staff/sergio-ocaña/objects/find.js
@@ -0,0 +1,14 @@
+find(object, search, callback, value){
+
+    for (var i = 0; i < object.length; i++) {
+        result = callback(object[i], search, value)
+    }
+}
+
+var callback = function (element, search, value) {
+    if (typeof search === "number")
+        if (search ===)
+            if (typeof search === string)
+
+}
+// TODO 
