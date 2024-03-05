da206405cafd21694ba29bbd433be299db1c5fa8
Strider <adrian.martinez89@gmail.com>
2024-03-05 13:14:48 +0100

    Añadiendo Callback y funciones de filtrado file #89

--- staff/adrian-martinez/objects/forEach.js
+++ staff/adrian-martinez/objects/forEach.js
@@ -1,46 +1,47 @@
 
-function forEach(object, callback){
-    
-    for(let i=0;i < object.length;i++){
+//Recorremos varios objectos. 
+//El segundo parámetro es un callback, que es una función anónima
+function forEach(object, callback) {
+
+    for (let i = 0; i < object.length; i++) {
 
         callback(object[i]);
     }
 }
+
 var users = {
     "0": {
         name: "Wendy",
         userName: "Wendy12",
         password: "123",
-        length: 0
     },
     "1": {
         name: "Peter Pan",
         userName: "PeterP",
         password: "123",
-        length: 1
     },
     "2": {
         name: "Campanilla",
         userName: "Campana",
         password: "123",
-        length: 2
-    }
+    },
+    length: 3
 }
-    //Callback
-    /* var userNames = function(obj){
+forEach(users, function (element) {
 
-        return users.name;
-    } */
+    console.log(element.name);
+});
 
-    var nums = {
-        0:5,
-        1:10,
-        2:20,
-        3:30,
-        length: 4
-    }
+var nums = {
+    0: 5,
+    1: 10,
+    2: 20,
+    3: 30,
+    length: 4
+}
 
-    forEach(nums, function(element) {
+//Recorremos un objeto
+forEach(nums, function (element) {
 
-        console.log(element * 10);
-    });
+    console.log(element * 10);
+});
