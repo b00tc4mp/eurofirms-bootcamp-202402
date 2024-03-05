556f0b4e7261ed62a9caecc45e7eb5a50c7232bc
Bernat Alcudia <bernatalcudia@gmail.com>
2024-03-05 14:32:14 +0100

    add file forEach #51

---
+++ staff/bernat-alcudia/objects/forEach.js
@@ -0,0 +1,13 @@
+// Función a ejecutar por cada elemento, que recibe tres argumentos:
+
+// currentValue
+// El elemento actual siendo procesado en el array.
+
+// index Opcional
+// El índice del elemento actual siendo procesado en el array.
+
+// array Opcional
+// El vector en el que forEach() esta siendo aplicado.
+
+// thisArg Opcional
+// Valor que se usará como this cuando se ejecute el callback.
