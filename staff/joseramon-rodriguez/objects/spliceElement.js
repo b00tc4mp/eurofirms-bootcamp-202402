07dd4b7f1bae2a231446106106393bfe15073f37
jose ramon rodriguez <jrjosera1@gmail.com>
2024-03-06 14:20:29 +0100

    rename map to mapElement,add reduceElement and spliceElement->WIP #50

---
+++ staff/joseramon-rodriguez/objects/spliceElement.js
@@ -0,0 +1,20 @@
+function spliceElement(object, start, deleteCount, items) {
+    var result = { length: 0 }
+    var startIndex = start
+    if (-object.length <= start < 0)
+        startIndex = start + object.length
+    if (start < -object.length)
+        startIndex = 0
+    if (start >= object.length) {
+        //add elements to objects
+    }
+    //comprobar si existe start
+    if ()
+
+
+
+
+
+
+        return result
+}
