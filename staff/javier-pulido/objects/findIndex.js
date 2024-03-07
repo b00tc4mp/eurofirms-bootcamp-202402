f0a65ad4c16eb6fb9799417c5fe5bcf6629266fd
Javier Pulido <jpdgranada@gmail.com>
2024-03-07 14:10:15 +0100

    add code in every, findFindex, some, splice #48

---
+++ staff/javier-pulido/objects/findIndex.js
@@ -0,0 +1,33 @@
+var people = {
+    0: { name: 'Salva', lastname: 'Tortajada', city: 'Manises' },
+    1:  { name: 'Maite', lastname: 'Albenca', city: 'Manises' },
+    2: { name: 'Javi', lastname: 'Pulido', city: 'Granada'}, 
+    3: { name: 'Pepito', lastname: 'Grillo', city: 'Pinocholandia'},
+    length:4
+}
+
+//Una función a ejecutar para cada elemento de la matriz. Debería devolver un valor verdadero para indicar que se ha encontrado un elemento coincidente y un valor falso en caso contrario.
+
+
+function findIndex (object, callback){
+
+    for (var i = 0; i < object.length; i++) {
+
+    if(callback(object[i])) {
+        return i
+    }
+
+}
+
+        return -1
+
+}
+
+var foundIndex = findIndex(people, function(element){
+
+
+    return element.name === 'Javi'
+
+})
+
+console.log(foundIndex)
