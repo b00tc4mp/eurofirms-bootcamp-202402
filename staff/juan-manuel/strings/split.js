06d9746e1a4dcc5f351b7bfb6b24f32fb1a83ced
internautavj001 <internautavj001@gmail.com>
2024-03-11 20:50:00 +0100

    Iterate on strings #149

--- staff/juan-manuel/strings/split.js
+++ staff/juan-manuel/strings/split.js
@@ -1,3 +1,3 @@
-var mascota = "Queen - 7 - Chihuahua"
-var mascotainfo = mascota.split(' - ')
-console.info({Raza: mascotainfo[2], Edad: mascotainfo[1], Nombre: mascotainfo[0]})
+var mascota = "Queen / 7 / Chihuahua"
+var mascotaInfo = mascota.split(' / ')
+console.info({Nombre: mascotaInfo[0], Edad: mascotaInfo[1], Raza: mascotaInfo[2]})
