ad7aa35f6b931f26f77eabdd9e9073bb95f45d87
cristina soriano <cristina.soris@gmail.com>
2024-03-11 01:28:58 +0100

    modify map.js and forEach.js #43

--- staff/cris-sori/objects/forEach.js
+++ staff/cris-sori/objects/forEach.js
@@ -1,5 +1,29 @@
+// /**
+//  * 
+//  */
+// function forEach(iterable, callback) {
+//     for (var i = 0; i < iterable.length; i++) {
+//         var element = iterable[i]
+
+//         callback(element)
+//     }
+// }
+
+// console.log(CASE 1: iterar en los numeros para multiplicar en la consola)
+
+// var a = [10, 20, 30, 40, 50]
+
+// forEach(nums, function (element {
+//     console.log(element * 10)
+// }))
+// // 100
+// // 200
+// // 300
+// // 400
+// // 500
+
 /**
- * 
+ * Loops on each value from iterable object.
  */
 function forEach(iterable, callback) {
     for (var i = 0; i < iterable.length; i++) {
@@ -9,15 +33,44 @@ function forEach(iterable, callback) {
     }
 }
 
-console.log(CASE 1: iterar en los numeros para multiplicar en la consola)
+console.log('TEST forEach')
 
-var a = [10, 20, 30, 40, 50]
+console.log('CASE 1: iterate on nums to show each one multiplied in the console')
 
-forEach(nums, function (element {
+var nums = {
+    0: 10,
+    1: 20,
+    2: 30,
+    3: 40,
+    4: 50,
+    length: 5
+}
+
+forEach(nums, function (element) {
     console.log(element * 10)
-}))
+})
 // 100
 // 200
 // 300
 // 400
 // 500
+
+console.log('CASE 2: iterate on nums to put each one multiplied by 10 in a new array')
+
+var nums = {
+    0: 10,
+    1: 20,
+    2: 30,
+    3: 40,
+    4: 50,
+    length: 5
+}
+var numsX10 = { length: 0 }
+
+forEach(nums, function (element) {
+    numsX10[numsX10.length] = element * 10
+    numsX10.length++
+})
+
+console.log(numsX10)
+// 100, 200, 300, 400, 500
