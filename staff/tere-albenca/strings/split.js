d1b3d3fdfe30b5c08cccdf8ce687d2896a84d57b
alalluna <terealbenca@gmail.com>
2024-03-11 18:45:03 +0100

    review split #147

--- staff/tere-albenca/strings/split.js
+++ staff/tere-albenca/strings/split.js
@@ -4,22 +4,23 @@ function split(string, separator, limit) {
     var separatorCount = 0;
     
     for (var i = 0; i < string.length; i++) {
-        //comprobar si ha alcanzado el limite y si el separeor count es mayor o igual que el limite menos uno
+        //comprobar si ha alcanzado el limite y si el separatorCount es mayor o igual que el limite final
         if (limit && separatorCount >= limit - 1) {
-            // Si se ha alcanzado el límite de divisiones permitidas, se agrega el resto
+            // Si se ha alcanzado el límite del patrón, se agrega el resto en el último string del array
             result.push(string.substring(i));
             break;
         }
-
+        //si el valor del caracter es igual a separator, hace push del string al arrray y pone en blanco el nuevo string, suma un count
         if (string[i] === separator) {
             result.push(accumulatedString);
             accumulatedString = '';
             separatorCount++;
+            //si no acumula el caracter al string 
         } else {
             accumulatedString += string[i];
         }
     }
-
+    //si accumulatedString no es un string vacion se devuelve dentro del array
     if (accumulatedString !== '') {
         result.push(accumulatedString);
     }
