d75a886ec05d4b31347aebbf819817b30820af9e
Ana María Moya <anamml461@gmail.com>
2024-03-11 10:03:38 +0100

    add changes in data of liveshare #63

--- staff/ana-maria-moya/liveshare/app/data.js
+++ staff/ana-maria-moya/liveshare/app/data.js
@@ -18,89 +18,88 @@ var data = (function () {
         localStorage.messages = JSON.stringify(messages)
     }
 
-}
     // data
 
     function findUser(callback) {
-    var users = loadUsers()
+        var users = loadUsers()
 
-    for (var i = 0; i < users.length; i++) {
-        var user = users[i]
+        for (var i = 0; i < users.length; i++) {
+            var user = users[i]
 
-        var matches = callback(user)
+            var matches = callback(user)
 
-        if (matches) return user
+            if (matches) return user
+        }
     }
-}
 
-function insertUser(user) {
-    var users = loadUsers()
-    user.id = parseInt(Math.random() * 1000000000000000000).toString(36)
+    function insertUser(user) {
+        var users = loadUsers()
+        user.id = parseInt(Math.random() * 1000000000000000000).toString(36)
 
-    users[users.length] = user
-    saveUsers(users)
+        users[users.length] = user
+        saveUsers(users)
 
-}
+    }
 
-function updateUser(user) {
-    var users = loadUsers()
+    function updateUser(user) {
+        var users = loadUsers()
 
-    var index = users.findIndex(function (user2) {
-        return user2.id === user.id
-    })
+        var index = users.findIndex(function (user2) {
+            return user2.id === user.id
+        })
 
-    users.splice(index, 1, user)
+        users.splice(index, 1, user)
 
-    saveUsers(users)
-}
+        saveUsers(users)
+    }
 
-function findUsers(callback) {
-    var users = loadUsers()
+    function findUsers(callback) {
+        var users = loadUsers()
 
-    var filtered = users.filter(callback)
+        var filtered = users.filter(callback)
 
-    return filtered
-}
+        return filtered
+    }
 
-function printUser() {
-    var users = loadUsers()
+    function printUsers() {
+        var users = loadUsers()
 
-    console.table(users)
-}
+        console.table(users)
+    }
 
-function getAllUsers() {
-    var users = loadUsers()
-    return users
-}
+    function getAllUsers() {
+        var users = loadUsers()
+        return users
+    }
 
-function printMessages() {
-    var messages = loadMessages()
+    function printMessages() {
+        var messages = loadMessages()
 
-    console.table(messages)
-}
-function insertMessage(message) {
-    var messages = loadMessages()
+        console.table(messages)
+    }
+    function insertMessage(message) {
+        var messages = loadMessages()
 
-    messages.push(message)
-    saveMessages(messages)
-}
+        messages.push(message)
+        saveMessages(messages)
+    }
 
-function findMessages(callback) {
-    var messages = loadMessages()
+    function findMessages(callback) {
+        var messages = loadMessages()
 
-    var filtered = messages.filter(callback)
-    return filtered
-}
+        var filtered = messages.filter(callback)
+        return filtered
+    }
 
-return {
-    findUser: findUser,
-    insertUser: insertUser,
-    updateUser: updateUser,
-    findUsers: findUsers,
-    printUsers: printUsers,
-    getAllUsers: getAllUsers,
-    printMessages: printMessages,
-    insertMessage: insertMessage,
-    findMessages: findMessages
-}
+    return {
+        findUser: findUser,
+        insertUser: insertUser,
+        updateUser: updateUser,
+        findUsers: findUsers,
+        printUsers: printUsers,
+        getAllUsers: getAllUsers,
+        printMessages: printMessages,
+        insertMessage: insertMessage,
+        findMessages: findMessages
+    }
 })()
