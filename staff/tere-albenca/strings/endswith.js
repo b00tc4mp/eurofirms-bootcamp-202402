677c2a83307c2f7cd398d8632fe831a844768740
alalluna <terealbenca@gmail.com>
2024-03-12 15:16:24 +0100

    review split and endsWith #147

--- staff/tere-albenca/strings/endswith.js
+++ staff/tere-albenca/strings/endswith.js
@@ -3,22 +3,22 @@ console.log('CASE 1: endsWith in string fruits')
 
 function endsWith(string, endPosition) {
     var result = ''
-    
-    if(typeof endPosition === 'undefined'||endPosition > string.length){
+
+    if (typeof endPosition === 'undefined' || endPosition > string.length) {
         endPosition = string.length
     }
 
-    for (var i = endPosition-1; i >= 0; i--) {
-            if (string[i] !== ' '){
-                result = string[i] +result
-            }
-                
-            else {
-                break
-            }  
+    for (var i = endPosition - 1; i >= 0; i--) {
+        if (string[i] !== ' ') {
+            result = string[i] + result
         }
-        return result
+
+        else {
+            break
+        }
     }
+    return result
+}
 
 var string = 'Tomate, Banana, strawberry, potato, bread, milk, Water, pepper'
 var endsFruits = endsWith(string)
--- staff/tere-albenca/strings/split.js
+++ staff/tere-albenca/strings/split.js
@@ -1,37 +1,35 @@
 function split(string, separator, limit) {
     var result = [];
-    var accumulatedString = '';
+    var accumulatedString = ''
     var separatorCount = 0;
-    
+
     for (var i = 0; i < string.length; i++) {
-        //comprobar si ha alcanzado el limite y si el separatorCount es mayor o igual que el limite final
-        if (limit && separatorCount >= limit - 1) {
-            // Si se ha alcanzado el límite del patrón, se agrega el resto en el último string del array
-            result.push(string.substring(i));
-            break;
-        }
-        //si el valor del caracter es igual a separator, hace push del string al arrray y pone en blanco el nuevo string, suma un count
-        if (string[i] === separator) {
-            result.push(accumulatedString);
-            accumulatedString = '';
-            separatorCount++;
+        var character = string[i]
+        //si el valor del caracter es igual a separator, se suma al array y pone en blanco el nuevo string, y suma un count
+        if (character === separator) {
+            result[result.length] = accumulatedString;
+            accumulatedString = ''
+            separatorCount++
             //si no acumula el caracter al string 
+            if (separatorCount >= limit)
+                break
+            
         } else {
-            accumulatedString += string[i];
+            accumulatedString += character
         }
     }
-    //si accumulatedString no es un string vacion se devuelve dentro del array
+    //si accumulatedString no es un string vacio se devuelve dentro del array
     if (accumulatedString !== '') {
-        result.push(accumulatedString);
+        result[result.length] = accumulatedString
     }
 
-    return result;
+    return result
 }
 
-console.log('function split');
-console.log('CASE 1');
+console.log('function split')
+console.log('CASE 1')
 
-var string = 'Hello world I am Maite';
-var result = split(string, ' ', 5)
-console.log(result);
-console.log({ expected: ['Hello', 'world', 'I', 'am', 'Maite'] })
+var string = 'Hello world I am Maite'
+var result = split(string, ' ', 3)
+console.log(result)
+console.log({ expected: ['Hello', 'world', 'I'] })
