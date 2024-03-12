1dbb07bd1e6fa38528665dd679cb5573c1c12c23
Yeyusin <sergioocruiz@gmail.com>
2024-03-12 14:07:13 +0100

    final version split with limit #142

--- staff/sergio-ocaña/strings/split.js
+++ staff/sergio-ocaña/strings/split.js
@@ -3,44 +3,40 @@ function split(string, separator, limit = string.length) {
     var add = ""
     var test = ""
 
-    for (var j = 0; j < limit; j++) {
+    for (var j = 0; j < string.length && limit > result.length; j++) {
 
-        if (separator.length > 1) {
-            if (separator[0] === string[j]) {
-                for (var i = 0; i < separator.length; i++) {
-                    test += string[i + j]
-                    if (string[j + i] !== separator[i]) {
-                        add += test
-                        test = ""
-                        j += i + 1
-                        break
+        if (separator.length > 1 && separator[0] === string[j]) {
 
-                    } if (separator === test) {
-                        test = ""
-                        result.push(add)
-                        add = ""
-                        j += separator.length
-                    }
+            for (var i = 0; i < separator.length; i++) {
+                test += string[i + j]
+                if (string[j + i] !== separator[i]) {
+                    add += test
+                    test = ""
+                    j += i + 1
+                    break
+
+                } if (separator === test) {
+                    test = ""
+                    result[result.length] = add
+                    add = ""
+                    j += separator.length
                 }
             }
+
         }
         if (separator === '')
             result.push(string[j])
 
         if (string[j] === separator) {
 
-            result.push(add)
+            result[result.length] = add
             add = ""
         }
         else {
             add += string[j]
         }
     }
-    if (separator.length > 1) {
-        result.push(add)
-        add = ""
-    }
-    if (separator !== "" && add !== '') { result.push(add) }
+    if (separator !== "" && add !== '') { result[result.length] = add }
     return result
 }
 var sssssssssssssstring = "The morning is upon us. Be aware about it"
