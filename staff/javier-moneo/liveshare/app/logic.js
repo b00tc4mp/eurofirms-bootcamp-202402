691774544360a619219b823e98683299a1c00f56
Javier Moneo <jvr.moneo@gmail.com>
2024-03-12 18:33:53 +0100

    add autorefresh for messages list; add submit for form to send message; #66

--- staff/javier-moneo/liveshare/app/logic.js
+++ staff/javier-moneo/liveshare/app/logic.js
@@ -1,44 +1,6 @@
 // business layer (logic)
 
 var logic = (function () {
-  // utils
-
-  function convertDateToISOString(date) {
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var millis = date.getMilliseconds();
-
-    function twoDigits(value) {
-      return value < 10 ? '0' + value : '' + value;
-    }
-
-    function threeDigits(value) {
-      return value < 10 ? '00' + value : value < 100 ? '0' + value : '' + value;
-    }
-
-    var isoDate =
-      year +
-      '-' +
-      twoDigits(month) +
-      '-' +
-      twoDigits(day) +
-      ' ' +
-      twoDigits(hours) +
-      ':' +
-      twoDigits(minutes) +
-      ':' +
-      twoDigits(seconds) +
-      '.' +
-      threeDigits(millis);
-
-    return isoDate;
-  }
-
   // helpers
 
   function validateName(name) {
@@ -114,7 +76,6 @@ var logic = (function () {
 
   function validateText(text) {
     if (typeof text !== 'string') throw new Error('text is not a string');
-    if (text.includes(' ')) throw new Error('text has spaces');
     if (!text.length) throw new Error('text is empty');
   }
 
@@ -218,7 +179,7 @@ var logic = (function () {
       from: sessionStorage.userId,
       to: userId,
       text: text,
-      date: convertDateToISOString(new Date()),
+      date: new Date().toISOString(),
     };
 
     data.insertMessage(message);
@@ -237,6 +198,10 @@ var logic = (function () {
     return messages;
   }
 
+  function getLoggedInUserId() {
+    return sessionStorage.userId;
+  }
+
   return {
     registerUser: registerUser,
     loginUser: loginUser,
@@ -245,5 +210,6 @@ var logic = (function () {
     retrieveUsers: retrieveUsers,
     sendMessageToUser: sendMessageToUser,
     retrieveMessagesWithUser: retrieveMessagesWithUser,
+    getLoggedInUserId: getLoggedInUserId,
   };
 })();
