f4007d2da8a1cda28e016850f49f14ea7d40a5de
alalluna <terealbenca@gmail.com>
2024-03-13 14:43:45 +0100

    repare  endswith #147

--- staff/tere-albenca/strings/endswith.js
+++ staff/tere-albenca/strings/endswith.js
@@ -1,27 +1,63 @@
 console.log('FUNCTION ENDSWITH')
 console.log('CASE 1: endsWith in string fruits')
+function endsWith(string, searchString, position) {
+    var length = string.length;
+    var searchStringLength = searchString.length;
 
-function endsWith(string, endPosition) {
-    var result = ''
-
-    if (typeof endPosition === 'undefined' || endPosition > string.length) {
-        endPosition = string.length
+    // Si la posición no es un número válido o es mayor que la longitud, la posición será igual a length.
+    if (typeof position !== 'number' || position !== position || position > length) {
+        position =length;
     }
 
-    for (var i = endPosition - 1; i >= 0; i--) {
-        if (string[i] !== ' ') {
-            result = string[i] + result
-        }
+    // Calculamos la posición de inicio de la búsqueda.
+    var startIndex = position - searchStringLength;
+    startIndex = Math.max(0, startIndex); // Aseguramos que startIndex no sea negativo.
 
-        else {
-            break
+    // Iteramos sobre la cadena desde startIndex hasta el principio.
+    for (var i = startIndex; i >= 0; i--) {
+        var found = true;
+        // Comparamos cada carácter de la subcadena con la cadena principal.
+        for (var j = 0; j < searchStringLength; j++) {
+            if (string[i + j] !== searchString[j]) {
+                found = false;
+                break;
+            }
+        }
+        // Si encontramos la subcadena, devolvemos true.
+        if (found) {
+            return true;
         }
     }
-    return result
+
+    // Si no se encontró la subcadena, devolvemos false.
+    return false;
 }
 
-var string = 'Tomate, Banana, strawberry, potato, bread, milk, Water, pepper'
-var endsFruits = endsWith(string)
+var stringFruits = 'Tomate, Banana, strawberry, potato, bread, milk, Water, pepper';
 
-console.log(string)
-console.log(endsFruits)
+console.log(endsWith(stringFruits, 'pepper'));
+// Expected output: true
+
+console.log(endsWith(stringFruits, 'pepper!', 17));
+
+console.log(endsWith(stringFruits, 'pepper '));
+// Expected output: false
+
+// function endsWith(string, endPosition) {
+//     var result = ''
+
+//     if (typeof endPosition === 'undefined' || endPosition > string.length) {
+//         endPosition = string.length
+//     }
+
+//     for (var i = endPosition - 1; i >= 0; i--) {
+//         if (string[i] !== ' ') {
+//             result = string[i] + result
+//         }
+
+//         else {
+//             break
+//         }
+//     }
+//     return result
+// }
