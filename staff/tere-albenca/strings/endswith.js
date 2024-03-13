32cb5e6564bc2aa24b6d3c2d0e43bd3f90ce43d3
alalluna <terealbenca@gmail.com>
2024-03-13 20:22:02 +0100

    review endswith #147

--- staff/tere-albenca/strings/endswith.js
+++ staff/tere-albenca/strings/endswith.js
@@ -1,36 +1,36 @@
 console.log('FUNCTION ENDSWITH')
 console.log('CASE 1: endsWith in string fruits')
 function endsWith(string, searchString, position) {
-    var length = string.length;
-    var searchStringLength = searchString.length;
+    var length = string.length
+    var searchStringLength = searchString.length
 
     // Si la posición no es un número válido o es mayor que la longitud, la posición será igual a length.
     if (typeof position !== 'number' || position !== position || position > length) {
-        position =length;
+        position =length
     }
 
     // Calculamos la posición de inicio de la búsqueda.
-    var startIndex = position - searchStringLength;
-    startIndex = Math.max(0, startIndex); // Aseguramos que startIndex no sea negativo.
+    var startIndex = position - searchStringLength
+    startIndex = startIndex < 0 ? 0 : startIndex // Aseguramos que startIndex no sea negativo.
 
     // Iteramos sobre la cadena desde startIndex hasta el principio.
     for (var i = startIndex; i >= 0; i--) {
-        var found = true;
+        var found = true
         // Comparamos cada carácter de la subcadena con la cadena principal.
         for (var j = 0; j < searchStringLength; j++) {
             if (string[i + j] !== searchString[j]) {
-                found = false;
-                break;
+                found = false
+                break
             }
         }
         // Si encontramos la subcadena, devolvemos true.
         if (found) {
-            return true;
+            return true
         }
     }
 
     // Si no se encontró la subcadena, devolvemos false.
-    return false;
+    return false
 }
 
 var stringFruits = 'Tomate, Banana, strawberry, potato, bread, milk, Water, pepper';
