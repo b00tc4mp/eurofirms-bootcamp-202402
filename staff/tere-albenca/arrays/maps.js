8b090a199045c8a3a5c5a326cc36e53ee8da5fd4
alalluna <terealbenca@gmail.com>
2024-03-14 15:12:25 +0100

    beginning on arrays #168

---
+++ staff/tere-albenca/arrays/maps.js
@@ -0,0 +1,56 @@
+
+var numbers =[10,20,30,40,50,60]
+//multiplicando *2
+//const case1 = numbers.map(x)function(x*2){
+
+var caseOne = numbers.map(function(x){
+    return x *2
+})
+console.log(caseOne)
+//crear un nuevo array con la longitud de cada uno de los strings
+
+var strings= ['hola','mundo','a','todos']
+
+var casetwo = strings.map(function(x){
+    return x.length
+})
+
+//crea un nuevo array con el resto de 2 de cada uno de los nuevos numeros
+
+var numberstwo = [12,40,6,7,24]
+var casethree = numberstwo.map(function(x){
+    return x % 2
+})
+//crea un nuevo array que contenga los email de los usuarios como strings
+var user = [
+    {name:'pepito', email:'pepito@grillo.com'},
+    {name:'wendy', email: 'wendy@darling.com'},
+    {name:'peter', email:'peter@pan.com'},
+    {name:'pinocho',email:'pinn@ocho.com'}
+]
+var casefour = user.map(function(x){
+    return x.email
+})
+//que contenga la ciudad de los usuarios como string
+var users= [
+    {name:'Pepito', information:{
+        city:'Madrid',
+        number:'65787959'
+    }},
+    {name:'Wendy', information:{
+        city:'Barcelona',
+        number:'66645982'
+    }},
+    {name:'Peter', information:{
+        city:'Sevilla',
+        number:'699774251'}},
+    {name:'Pinocho', information:{
+        city:'Valencia',number:'669574343'}}
+]
+
+
+
+
+
+
+ 
