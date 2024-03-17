35e4ad66ea9bea4b5e36d2a38e1a0ff0769d46f9
Ruben Sanchez <rubensala1980@gmail.com>
2024-03-17 12:11:34 +0100

    add 2nd transcription of several archives #68

---
+++ staff/ruben-sanchez/liveshare/app/logic2.js
@@ -0,0 +1,16 @@
+var logic = (function () {
+    // helpers
+
+    function validateName(name) {
+        if (name.length < 1)
+            throw new Error('name is lower than 1 character')
+
+        var nameIsBlank = true
+
+        for (var i = 0; i<name.length)
+
+    }
+
+
+
+})
