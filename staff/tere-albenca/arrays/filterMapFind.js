d4fe6ed8153a22758effb1d5644eff29f299d924
alalluna <terealbenca@gmail.com>
2024-03-17 20:06:22 +0100

    create sliceorpush #168

--- staff/tere-albenca/arrays/filterMapFind.js
+++ staff/tere-albenca/arrays/filterMapFind.js
@@ -3,60 +3,63 @@
 //una vez tengas un usuario, en un nuevo array guardamos los mensajes que le ha enviado a Wendy (filter)
 //con esta informacion creamos un nuevo array que contenga solo el text de los mensajes, como un string(map)
 var users = [
-    {
-        name:'wendy', 
-        email: 'wendy@darling.com', 
-        messages:[
-            {from: 'wendy', to:'pepito', text: 'hola'},
-            { from: 'wendy', to: 'pepito', text: 'Bien y tu?' },
-            { from: 'wendy', to: 'pepito', text: 'Me alegro!' }
+  {
+    name: "wendy",
+    email: "wendy@darling.com",
+    messages: [
+      { from: "wendy", to: "pepito", text: "hola" },
+      { from: "wendy", to: "pepito", text: "Bien y tu?" },
+      { from: "wendy", to: "pepito", text: "Me alegro!" },
+    ],
+  },
+  {
+    name: "pepito",
+    email: "pepito@grillo.com",
+    messages: [
+      { from: "pepito", to: "wendy", text: "hola" },
+      { from: "pepito", to: "wendy", text: "Como estás?" },
+      { from: "pepito", to: "wendy", text: "Bien tambien" },
+      { from: "pepito", to: "peter", text: "Hola" },
+      { from: "pepito", to: "peter", text: "Todo bien!" },
+    ],
+  },
+  {
+    name: "peter",
+    email: "peter@pan.com",
+    messages: [
+      { from: "peter", to: "pepito", text: "hola" },
+      { from: "peter", to: "pepito", text: "Que tal?" },
+    ],
+  },
+];
+var finduser = users.find(function (user) {
+  return user.email === "pepito@grillo.com";
+});
+console.log(finduser);
+var filtermessages = finduser.messages.filter(function (message) {
+  return message.to === "wendy";
+});
+console.log(filtermessages);
+var contentMessages = filtermessages.map(function (message) {
+  return message.text;
+});
+console.log(contentMessages);
 
-        ]
-    },
-    {
-        name: 'pepito' , 
-        email:'pepito@grillo.com', 
-        messages: [
-            {from:'pepito', to: 'wendy', text:'hola'},
-            {from:'pepito', to: 'wendy', text :'Como estás?'},
-            { from: 'pepito', to: 'wendy', text: 'Bien tambien' },
-            { from: 'pepito', to: 'peter', text: 'Hola' },
-            { from: 'pepito', to: 'peter', text: 'Todo bien!' }
-        ]
-    },
-    {
-        name:'peter', 
-        email: 'peter@pan.com', 
-        messages: [
-            {from: 'peter', to: 'pepito', text: 'hola'},
-            {from: 'peter', to: 'pepito', text:'Que tal?'}
-        ]
-    }
-]
-var finduser= users.find(function(user){
-    return user.email === 'pepito@grillo.com'
-})
-console.log(finduser)
-var filtermessages = finduser.messages.filter(function(message){
-return message.to === 'wendy'
-})
-console.log(filtermessages)
-var contentMessages = filtermessages.map(function(message){
-    return message.text
-})
-console.log(contentMessages)
+var tests = users
+  .find(function (user) {
+    return user.email === "pepito@grillo.com";
+  })
+  .messages.filter(function (message) {
+    return message.to === "wendy";
+  })
+  .map(function (message) {
+    return message.text;
+  });
+console.log(tests);
 
-var tests =users.find(function(user){
-    return user.email === 'pepito@grillo.com'
-}).messages.filter (function(message){
-    return message.to==='wendy'
-}).map(function(message){
-    return message.text
-})
-console.log(tests)
+var texts = users
+  .find((user) => user.email === "pepito@grillo.com")
+  .messages.filter((message) => message.to === "wendy")
+  .map((message) => message.text);
 
-var texts = users.find(user => user.email === 'pepito@grillo.com')
-.messages.filter (message => message.to ==='wendy')
-.map (message => message.text )
-
-console.log(texts)
+console.log(texts);
--- staff/tere-albenca/arrays/find.js
+++ staff/tere-albenca/arrays/find.js
@@ -1,77 +1,73 @@
 // usando el find, encuentra el primer numero mayor a 20
 
-var numbers = [10, 20, 30, 40, 50, 60]
-var caseOne = numbers.find(function(element){
-    return element > 20
-})
-console.log(caseOne)
-
-// --------------------------------------------------
+var numbers = [10, 20, 30, 40, 50, 60];
+var caseOne = numbers.find(function (element) {
+  return element > 20;
+});
+console.log(caseOne);
 
 // encuentra el primer string que contenga una 'u'
 
-var strings = ['hola', 'mundo', 'a', 'todos']
-var caseTwo = numbers.find(function(element){
-    return element.includes('u')
-})
-console.log(caseTwo)
-
-// --------------------------------------------------
+var strings = ["hola", "mundo", "a", "todos"];
+var caseTwo = numbers.find(function (element) {
+  return element.includes("u");
+});
+console.log(caseTwo);
 
 // encuentra el primer numero impar
 
-var numbers2 = [12, 40, 6, 7, 13]
-var caseThree = numbers2.find(function(element) {
-    return element % 2 !== 0
+var numbers2 = [12, 40, 6, 7, 13];
+var caseThree = numbers2.find(function (element) {
+  return element % 2 !== 0;
 });
-console.log(caseThree)
-
-// --------------------------------------------------
+console.log(caseThree);
 
 // encuentra el usuario con email 'wendy@darling.com'
 
 var users = [
-    { name: 'pepito', email: 'pepito@grillo.com' },
-    { name: 'wendy', email: 'wendy@darling.com' },
-    { name: 'peter', email: 'peter@pan.com' },
-    { name: 'pinocho', email: 'pin@ocho.com' },
-]
-var caseFour = users.find(function(element) {
-    return element.email === 'wendy@darling.com'
+  { name: "pepito", email: "pepito@grillo.com" },
+  { name: "wendy", email: "wendy@darling.com" },
+  { name: "peter", email: "peter@pan.com" },
+  { name: "pinocho", email: "pin@ocho.com" },
+];
+var caseFour = users.find(function (element) {
+  return element.email === "wendy@darling.com";
 });
-console.log(caseFour)
-
-// --------------------------------------------------
+console.log(caseFour);
 
 // encuentra el usuario con la ciudad 'Barcelona'
 
 var users2 = [
-    {
-        name: 'pepito', information: {
-            city: 'Madrid',
-            number: '65787959'
-        }
+  {
+    name: "pepito",
+    information: {
+      city: "Madrid",
+      number: "65787959",
     },
-    {
-        name: 'wendy', information: {
-            city: 'Barcelona',
-            number: '7869406'
-        }
+  },
+  {
+    name: "wendy",
+    information: {
+      city: "Barcelona",
+      number: "7869406",
     },
-    {
-        name: 'peter', information: {
-            city: 'Sevilla',
-            number: '78795040'
-        }
+  },
+  {
+    name: "peter",
+    information: {
+      city: "Sevilla",
+      number: "78795040",
     },
-    {
-        name: 'pinocho', information: {
-            city: 'Valencia',
-            number: '57694068'
-        }
-    }
-]
-var caseFive = users2.find(function(element) {
-    return element.information.city === 'Barcelona'
-})
-console.log(caseFive)
+  },
+  {
+    name: "pinocho",
+    information: {
+      city: "Valencia",
+      number: "57694068",
+    },
+  },
+];
+var caseFive = users2.find(function (element) {
+  return element.information.city === "Barcelona";
+});
+console.log(caseFive);
