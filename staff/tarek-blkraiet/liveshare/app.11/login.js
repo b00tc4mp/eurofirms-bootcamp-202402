8f2087ecc318474ddb8c750776078667e2be86e8
tarek blkraiet <tarekblkraiet@gmail,com>
2024-03-20 11:45:18 +0100

    add new folders #67

---
+++ staff/tarek-blkraiet/liveshare/app.11/login.js
@@ -0,0 +1,36 @@
+// presentation layer
+
+var form = document.querySelector('.form')
+
+form.onsubmit = function (event) {
+  event.preventDefault()
+
+  var usernameInput = form.querySelector('#username')
+  var username = usernameInput.value
+
+  var passwordInput = form.querySelector('#password')
+  var password = passwordInput.value
+
+  try {
+
+    logic.loginUser(username, password)
+
+    console.log('user logged in')
+
+    // alert('user logged in')
+
+    form.reset()
+    // para clean los especios despues de escribir
+    var loginAddress = location.href
+
+    var homeAddress = loginAddress.replace('login', 'home')
+
+    location.href = homeAddress
+
+
+  } catch (error) {
+    console.error(error.message)
+
+    alert(error.message)
+  }
+}
