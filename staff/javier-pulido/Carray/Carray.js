98f3d2a7e1f61f1a4a57b4322faed2f375e663a1
Javier Pulido <jpdgranada@gmail.com>
2024-03-22 10:58:09 +0100

    to resolve about create method with fake array called Carray, methods: reverse,push,join #192

--- staff/javier-pulido/Carray/Carray.js
+++ staff/javier-pulido/Carray/Carray.js
@@ -7,10 +7,10 @@ function Carray() {
         this[i] = element
     }
 
-    this.length = element.length
+    this.length = elements.length
 }
 
-Carray.prototype.array.forEach = function (callback) {
+Carray.prototype.forEach = function (callback) {
     for (var i = 0; i < this.length; i++) {
         var element = this[i]
 
@@ -23,7 +23,7 @@ Carray.prototype.find = function (callback) {
         var element = this[i]
 
         var matches = callback(element)
-        
+
         if (matches)
             return element
     }
@@ -43,5 +43,46 @@ Carray.prototype.map = function (callback) {
 
     return results
 }
+
+//TODO Revisar este ejercicio Reverse
+Carray.prototype.reverse = function () {
+    var iterations = Math.floor(this.length / 2)
+
+    for (var i = 0; i < iterations; i++) {
+        var element = this[i]
+        this[i] = this[this.length - 1 - i]
+
+        this[this.length - 1 - i] = element
+    }
+
+    return this
+}
+//--------------------------------------------------------------------------------------/
+Carray.prototype.push = function () {
+    for (var i = 0; i < arguments.length; i++) {
+        this[this.length] = arguments[i]
+        this.length++
+    }
+    return this.length
+}
+//--------------------------------------------------------------------------------------/
+Carray.prototype.join = function (separator = ',') {
+    var result = ''
+
+    for (var i = 0; i < this.length; i++) {
+        var element = this[i]
+        if (i !== 0) {
+
+            result = result + separator + element
+        }
+        else {
+            result = element
+        }
+    }
+    return result
+}
+
+
+
 
 module.exports = Carray 
--- staff/javier-pulido/Carray/Carray.test.js
+++ staff/javier-pulido/Carray/Carray.test.js
@@ -58,10 +58,10 @@ console.log('> find')
 console.log('CASE find person with age 20')
 
 var people = new Carray(
-    {name: 'Peter', age: 30 },
-    {name: 'Wendy', age: 25 },
-    {name: 'James', age: 20 },
-    {name: 'Campa', age: 15 }
+    { name: 'Peter', age: 30 },
+    { name: 'Wendy', age: 25 },
+    { name: 'James', age: 20 },
+    { name: 'Campa', age: 15 }
 )
 
 var person = people.find(function (person) {
@@ -75,9 +75,9 @@ console.log('> map')
 
 console.log('CASE maps products from cart to subtotals')
 
-var cart = new Carray (
+var cart = new Carray(
     { name: 'Socks Adidas', price: 20, quantity: 2 },
-    { name: 'Nike Air Max', price: 80, quantity: 1},
+    { name: 'Nike Air Max', price: 80, quantity: 1 },
     { name: 'Shorts Puma', price: 30, quantity: 4 }
 )
 
@@ -90,7 +90,7 @@ console.log(subtotals)
 
 console.log('CASE maps names to upper-case')
 
-var names = new Carray('Ana', 'Adrian', 'Javier', 'Sergio', 'Bernat', 'Maite', 'Sara' )
+var names = new Carray('Ana', 'Adrian', 'Javier', 'Sergio', 'Bernat', 'Maite', 'Sara')
 
 var namesInUpperCase = names.map(function (name) {
     return name.toUpperCase()
@@ -99,3 +99,44 @@ var namesInUpperCase = names.map(function (name) {
 console.log(namesInUpperCase)
 // Carray { 0: 'ANA', 1: 'ADRIAN', 2: 'JAVIER', 3: 'SERGIO', 4: 'BERNAT', 5: 'MAITE, 6: 'SARA', length: 7 }
 
+
+console.log('CASE reverse the colors')
+
+var colors = new Carray('red', 'blue', 'yellow', 'orange')
+
+var reverseColors = colors.reverse()
+
+console.log(reverseColors)
+
+// Carray { 0: 'orange', 1: 'yellow', 2: 'blue', 'red', length: 4}
+
+console.log('CASE add two numbers')
+
+var numbers = new Carray(10, 20, 30)
+
+var addNumbers = numbers.push(40, 50)
+
+console.log(addNumbers)
+console.log(numbers)
+
+// Carray { 10, 20, 30, 40, 50 length: 5}
+
+console.log('CASE join element from object into string')
+
+var names1 = new Carray ('Ana', 'Adrian', 'Javier', 'Sergio', 'Bernat', 'Maite', 'Sara')
+
+var namesJoin = names1.join('')
+
+console.log(namesJoin)
+
+//String 'AnaAdrianJavierSergioBernatMaiteSara'
+
+console.log('CASE join element from object into string')
+
+var names1 = new Carray ('Ana', 'Adrian', 'Javier', 'Sergio', 'Bernat', 'Maite', 'Sara')
+
+var namesJoin = names1.join()
+
+console.log(namesJoin)
+
+// Strin 'Ana,Adrian,Javier,Sergio,Bernat,Maite,Sara'
