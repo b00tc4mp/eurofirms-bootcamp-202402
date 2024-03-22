f4cb9724566b5addc15621724cd46e4c2c2f00af
Manuel Barzi <manuelbarzi@gmail.com>
2024-03-22 15:48:51 +0100

    add person shape and person constructor #195

--- staff/manuel-barzi/shapes/demo/index.js
+++ staff/manuel-barzi/shapes/demo/index.js
@@ -1,129 +1,138 @@
-(function () {
-    function Shape2D() {
-        this.width = 0
-        this.height = 0
-    }
+function Shape2D(width, height, color) {
+    this.width = width
+    this.height = height
+    this.color = color
 
-    Shape2D.prototype.getWidthInPixels = function () {
-        return this.width + 'px'
-    }
+    this.x = 0
+    this.y = 0
 
-    Shape2D.prototype.getHeightInPixels = function () {
-        return this.height + 'px'
-    }
+    var container = document.createElement('div')
+
+    container.style.position = 'absolute'
+    container.style.left = this.x + 'px'
+    container.style.top = this.y + 'px'
+    container.style.width = this.width + 'px'
+    container.style.height = this.height + 'px'
+    container.style.backgroundColor = this.color
+
+    this.container = container
+}
+
+// location
+
+Shape2D.prototype.setX = function (x) {
+    this.x = x
+
+    this.container.style.left = this.x + 'px'
+}
+
+Shape2D.prototype.getX = function () {
+    return this.x
+}
+
+Shape2D.prototype.setY = function (y) {
+    this.y = y
+
+    this.container.style.top = this.y + 'px'
+}
+
+Shape2D.prototype.getY = function () {
+    return this.y
+}
+
+Shape2D.prototype.setLocation = function (x, y) {
+    this.setX(x)
+    this.setY(y)
+}
+
+// dimensions
+
+Shape2D.prototype.setWidth = function (width) {
+    this.width = width
+
+    this.container.style.width = this.width + 'px'
+}
+
+Shape2D.prototype.getWidth = function () {
+    return this.width
+}
+
+Shape2D.prototype.setHeight = function (height) {
+    this.height = height
+
+    this.container.style.height = this.height + 'px'
+}
+
+Shape2D.prototype.getHeight = function () {
+    return this.height
+}
+
+Shape2D.prototype.setDimensions = function (width, height) {
+    this.setWidth(width)
+    this.setHeight(height)
+}
 
 
-    /*
-    var adrian = new Shape2D
-    
-    adrian.width = 60
-    adrian.height = 175
-    //adrian.depth = 30
-    
-    console.log(adrian.getWidthInPixels(), adrian.getHeightInPixels())
-    
-    
-    var box = document.createElement('div')
-    
-    box.style.width = adrian.getWidthInPixels()
-    box.style.height = adrian.getHeightInPixels()
-    box.style.backgroundColor = 'dodgerblue'
-    
-    document.body.appendChild(box)
-    */
+function Person() {
+    Shape2D.call(this, 60, 175, 'transparent')
 
-    function Shape3D(width, height, depth, color) {
-        this.width = width
-        this.height = height
-        this.depth = depth
-        this.color = color
+    var head = new Shape2D(20, 20, 'bisque')
 
-        var box = document.createElement('div')
-        box.id = 'box'
-        box.style.transformStyle = 'preserve-3d'
-        box.style.position = 'absolute'
-        box.style.left = '0px'
-        box.style.top = '0px'
-        box.style.width = this.getWidthInPixels()
-        box.style.height = this.getHeightInPixels()
-        box.style.backgroundColor = this.color
+    var leftArm = new Shape2D(10, 70, 'red')
+    leftArm.setY(head.getHeight())
 
-        var bottom = document.createElement('div')
-        bottom.id = 'bottom'
-        bottom.style.width = this.getWidthInPixels()
-        bottom.style.height = this.getHeightInPixels()
-        bottom.style.backgroundColor = this.color
-        bottom.style.transform = 'translate3d(0, 0, 0)'
-        bottom.style.opacity = 0.8
-        box.appendChild(bottom)
+    var body = new Shape2D(40, 80, 'dodgerblue')
+    body.setX(leftArm.getWidth())
+    body.setY(head.getHeight())
 
-        var top = document.createElement('div')
-        top.id = 'top'
-        top.style.width = this.getWidthInPixels()
-        top.style.height = this.getHeightInPixels()
-        top.style.backgroundColor = this.color
-        top.style.transform = 'translate3d(0, -' + this.getHeightInPixels() + ', ' + this.getDepthInPixels() + ')'
-        top.style.opacity = 0.8
-        box.appendChild(top)
+    head.setX(body.getX() + body.getWidth() / 2 - head.getWidth() / 2)
 
-        var left = document.createElement('div')
-        left.id = 'left'
-        left.style.height = this.getHeightInPixels()
-        left.style.width = this.getDepthInPixels()
-        left.style.backgroundColor = this.color
-        left.style.transform = 'translate3d(-' + this.depth / 2 + 'px, -' + 2 * this.height + 'px, ' + this.depth / 2 + 'px) rotate3d(0, 1, 0, 90deg)'
-        left.style.opacity = 0.8
-        box.appendChild(left)
+    var rightArm = new Shape2D(10, 70, 'red')
+    rightArm.setX(leftArm.getWidth() + body.getWidth())
+    rightArm.setY(head.getHeight())
 
-        var right = document.createElement('div')
-        right.id = 'right'
-        right.style.height = this.getHeightInPixels()
-        right.style.width = this.getDepthInPixels()
-        right.style.backgroundColor = this.color
-        right.style.transform = 'translate3d(' + (this.width - this.depth / 2) + 'px, -' + 3 * this.height + 'px, ' + this.depth / 2 + 'px) rotate3d(0, 1, 0, 90deg)'
-        right.style.opacity = 0.8
-        box.appendChild(right)
+    var leftLeg = new Shape2D(15, 75, 'red')
+    leftLeg.setX(leftArm.getWidth())
+    leftLeg.setY(head.getHeight() + body.getHeight())
 
-        this.container = box
-    }
+    var rightLeg = new Shape2D(15, 75, 'red')
+    rightLeg.setX(leftArm.getWidth() + body.getWidth() - rightLeg.getWidth())
+    rightLeg.setY(head.getHeight() + body.getHeight())
 
-    Shape3D.prototype.getWidthInPixels = function () {
-        return this.width + 'px'
-    }
+    this.container.appendChild(head.container)
+    this.container.appendChild(body.container)
+    this.container.appendChild(leftArm.container)
+    this.container.appendChild(rightArm.container)
+    this.container.appendChild(leftLeg.container)
+    this.container.appendChild(rightLeg.container)
+}
 
-    Shape3D.prototype.getHeightInPixels = function () {
-        return this.height + 'px'
-    }
+Person.prototype = Object.create(Shape2D.prototype)
+Person.prototype.constructor = Person
 
-    Shape3D.prototype.getDepthInPixels = function () {
-        return this.depth + 'px'
-    }
+// demo
 
-    Shape3D.prototype.paint = function (container) {
-        container.appendChild(this.container)
-    }
+var adrian = new Person
+adrian.setLocation(200, 100)
 
-    var space = document.createElement('div')
-    space.id = 'space'
-    space.style.position = 'absolute'
-    space.style.left = '0px'
-    space.style.top = '0px'
-    space.style.width = '100%'
-    space.style.backgroundColor = 'skyblue'
+var ana = new Person
+ana.setLocation(100, 100)
 
-    var adrian = new Shape3D(60, 175, 30, 'dodgerblue')
-    adrian.container.style.transform = 'translate(100px, 200px) rotate3d(1, 1, 0, -50deg)'
-    adrian.paint(space)
+document.body.appendChild(adrian.container)
+document.body.appendChild(ana.container)
 
-    var david = new Shape3D(65, 180, 40, 'black')
-    david.container.style.transform = 'translate(300px, 100px) rotate3d(1, 1, 0, -30deg)'
-    david.paint(space)
+// document.onkeydown = function (event) {
+//     console.log(event.key)
 
-    var jose = new Shape3D(60, 170, 35, 'blue')
-    jose.container.style.transform = 'translate(200px, 100px) rotate3d(1, 1, 0, -30deg)'
-    jose.paint(space)
+//     if (event.key === 'ArrowLeft')
+//         adrian.setX(adrian.getX() - 10)
+//     else if (event.key === 'ArrowRight')
+//         adrian.setX(adrian.getX() + 10)
+//     else if (event.key === 'ArrowUp')
+//         adrian.setY(adrian.getY() - 10)
+//     else if (event.key === 'ArrowDown')
+//         adrian.setY(adrian.getY() + 10)
 
-    document.body.appendChild(space)
-})()
+// }
 
 
