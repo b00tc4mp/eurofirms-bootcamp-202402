8f48562d08232fc6b0d8923ad11786aed45a71aa
Javier Moneo <jvr.moneo@gmail.com>
2024-03-25 20:03:38 +0100

    add implementation of hangman; #201

---
+++ staff/javier-moneo/shapes/hangman/Form.js
@@ -0,0 +1,10 @@
+function Form() {
+  Component.call(this, 'form');
+}
+
+Form.prototype = Object.create(Component.prototype);
+Form.prototype.constructor = Form;
+
+Form.prototype.onSubmit = function (callback) {
+  this.container.onsubmit = callback;
+};
---
+++ staff/javier-moneo/shapes/hangman/index.js
@@ -0,0 +1,20 @@
+var body = new Component();
+body.container = document.body;
+
+var title = new Component('h1');
+title.setText('Hangman');
+body.add(title);
+
+var startForm = new StartForm();
+
+startForm.onSubmit(function (words) {
+  sessionStorage.secret = words;
+
+  body.remove(startForm);
+
+  var charBoxes = new CharBoxes(words);
+
+  body.add(charBoxes);
+});
+
+body.add(startForm);
