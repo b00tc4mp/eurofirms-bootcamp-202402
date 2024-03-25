7e3cdd0018bb8eb2b6bd8c5fc9469d040b61bebc
Ana María Moya <anamml461@gmail.com>
2024-03-25 21:20:51 +0100

    do hangman with thw files #205

---
+++ staff/ana-maria-moya/shapes/hangman/input.js
@@ -0,0 +1,10 @@
+function Input() {
+    CompositionEvent.call(this, 'input')
+}
+
+Input.prototype = Object.create(Component.prototype)
+Input.prototype.constructor = Input
+
+Input.prototype.getValue = function () {
+    return this.container.value
+}
