1486aa602739c443a68fb5622cd09cfc50899c70
Bernat Alcudia <bernatalcudia@gmail.com>
2024-03-26 13:48:00 +0100

    add hangman files #203

---
+++ staff/bernat-alcudia/hangman/CharBoxes.js
@@ -0,0 +1,26 @@
+function CharBoxes(words) {
+    Component.call(this, 'div')
+
+    this.setStyle('display', 'flex')
+    this.setStyle('gap', '5px')
+    this.setStyle('margin', '5px')
+
+    for (var i = 0; i < words.length; i++) {
+        var char = words[i]
+
+        var charBox = new Component('div')
+        charBox.setStyle('border', '1px solid black')
+        charBox.setStyle('font family', 'courier')
+        charBox.setStyle('font-size', '36px')
+        charBox.setStyle('padding', '5px')
+        charBox.setStyle('background-color', 'black')
+        charBox.setStyle('min-width', '20px')
+        charBox.setText(char)
+
+        this.add(charBox)
+    }
+}
+
+
+CharBoxes.prototype = Object.create(Component.prototype)
+CharBoxes.prototype.constructor = CharBoxes
---
+++ staff/bernat-alcudia/hangman/Shape2D.js
@@ -0,0 +1,74 @@
+function Shape2D(width, height, color) {
+    Component.call(this, 'div')
+
+    this.width = width
+    this.height = height
+    this.color = color
+
+    this.x = 0
+    this.y = 0
+
+    this.container.style.position = 'absolute'
+    this.container.style.left = this.x + 'px'
+    this.container.style.top = this.y + 'px'
+    this.container.style.width = this.width + 'px'
+    this.container.style.height = this.height + 'px'
+    this.container.style.backgroundColor = this.color
+}
+
+Shape2D.prototype = Object.create(Component.prototype)
+Shape2D.prototype.constructor = Shape2D
+
+//location
+
+Shape2D.prototype.setX = function (x) {
+    this.x = x
+    this.container.style.left = this.x + 'px'
+}
+
+Shape2D.prototype.getX = function () {
+    return this.x
+}
+
+Shape2D.prototype.setY = function (y) {
+    this.y = y
+
+    this.container.style.top = this.y + 'px'
+}
+
+Shape2D.prototype.getY = function () {
+    return this.y
+}
+
+Shape2D.prototype.setLocation = function (x, y) {
+    this.setX(x)
+    this.setY(y)
+}
+
+
+//dimensions
+
+Shape2D.prototype.setWidth = function (width) {
+    this.width = width
+
+    this.container.style.width = this.width + 'px'
+}
+
+Shape2D.prototype.getWidth = function () {
+    return this.width
+}
+
+Shape2D.prototype.setHeight = function (height) {
+    this.height = height
+
+    this.container.style.height = this.height + 'px'
+}
+
+Shape2D.prototype.getHeight = function () {
+    return this.height
+}
+
+Shape2D.prototype.setDimensions = function (width, height) {
+    this.setWidth(width)
+    this.setHeight(height)
+}     
