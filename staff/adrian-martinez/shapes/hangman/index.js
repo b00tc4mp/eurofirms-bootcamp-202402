65b12e14767e711120d1150ab42a4cef9a64fa44
Strider <adrian.martinez89@gmail.com>
2024-03-27 23:14:03 +0100

    Add utimates files Hangman with JS 5 #212

--- staff/adrian-martinez/shapes/hangman/index.js
+++ staff/adrian-martinez/shapes/hangman/index.js
@@ -7,6 +7,8 @@ title.setText("Hangman");
 body.add(title);
 
 var startForm = new StartForm;
+var charBoxes;
+var hangman;
 
 startForm.onSubmit(function(words){
 
@@ -14,8 +16,72 @@ startForm.onSubmit(function(words){
 
     body.remove(startForm);
 
-    var charBoxes = new CharBoxes();
+    charBoxes = new CharBoxes(words);
+
+    hangman = new hangman(150, 200);
+    body.add(hangman);
 
     body.add(charBoxes);
+
+    body.add(guessForm);
+
+    startForm.reset();
 })
+
 body.add(startForm);
+
+var guessForm = new GuessForm;
+var failsCount = 0;
+var assertionsCount = 0;
+var charsUsed = [];
+
+guessForm.onSubmit(function(char){
+    if(!charsUsed.includes(char)){
+
+        charsUsed.push(char);
+
+        var secret = sessionStorage.secret;
+
+        for(var i=0;i < secret.length;i++){
+
+            var charToCompare = secret[i];
+            if(char === charToCompare){
+
+                charBoxes.showChar(i);
+                assertionsCount++;
+            }
+        }
+        if(assertionsCount === secret.length){
+
+            setTimeout(function(){
+
+                alert("You Win!!");
+
+                body.remove(guessForm);
+                body.remove(hangman);
+                body.remove(charBoxes);
+                body.add(startForm);
+            }, 1000)
+        }
+        if(!secret.includes(char)){
+            failsCount++;
+            if(failsCount > 6){
+
+                alert("GAME OVER. The solution was "+ sessionStorage.secret);
+    
+                body.remove(guessForm);
+                body.remove(hangman);
+                body.remove(charBoxes);
+                body.add(startForm);
+            }
+            else{
+    
+                hangman.setFails(failsCount);
+                console.log("Fallo");
+            }
+        }
+        guessForm.reset();    
+    }
+    else
+        alert("char was already used");
+})
