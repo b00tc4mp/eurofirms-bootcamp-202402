1588c63ece971d157beabf234725b3886fb3c1d1
Manuel Barzi <manuelbarzi@gmail.com>
2024-04-02 16:32:46 +0200

    separate pixel art in components #195

---
+++ staff/manuel-barzi/shapes/zzz/pixel-art.3/index.jsx
@@ -0,0 +1,111 @@
+class App extends React.Component {
+    constructor() {
+        super()
+
+        const board = new Array(10)
+
+        for (let i = 0; i < board.length; i++)
+            board[i] = new Array(board.length).fill('white')
+
+        this.state = {
+            board,
+            turn: 0,
+            color: 'black'
+        }
+    }
+
+    paint(row, column) {
+        const prevBoard = this.state.board
+
+        const newBoard = new Array(prevBoard.length)
+        for (let i = 0; i < prevBoard.length; i++)
+            newBoard[i] = new Array(prevBoard[0].length).fill('white')
+
+        for (const i in prevBoard)
+            for (const j in prevBoard[i])
+                newBoard[i][j] = prevBoard[i][j]
+
+
+        newBoard[row][column] = this.state.color
+
+        this.setState({ board: newBoard })
+    }
+
+    clear() {
+        const prevBoard = this.state.board
+
+        const newBoard = new Array(prevBoard.length)
+        for (let i = 0; i < prevBoard.length; i++)
+            newBoard[i] = new Array(prevBoard[0].length).fill('white')
+
+        this.setState({ board: newBoard })
+    }
+
+    render() {
+        const board = this.state.board
+
+        const colors = ['red', 'green', 'blue', 'yellow', 'white', 'black']
+
+        return <>
+            <header>
+                <h1>Pixel Art</h1>
+            </header>
+            <main className="main">
+                <ul className="colors">
+                    {colors.map(color => <li key={color}>
+                        <ColorButton
+                            color={color}
+                            onClick={() => this.setState({ color })}
+                        />
+                    </li>)}
+                </ul>
+
+                <section className="board" style={{
+                    gridTemplateColumns: `repeat(${board.length}, 1fr)`,
+                    gridTemplateRows: `repeat(${board.length}, 1fr)`
+                }}>
+                    {board.map((row, i) => row.map((column, j) => <Pixel
+                        key={`${i}-${j}`}
+                        color={board[i][j]}
+                        onClick={() => this.paint(i, j)}
+                    />)).flat(Infinity)}
+                </section>
+
+                <button onClick={() => this.clear()}>reset</button>
+            </main>
+            <footer></footer>
+        </>
+    }
+}
+
+class Pixel extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return <div
+            className="pixel"
+            style={{ backgroundColor: this.props.color }}
+            onClick={this.props.onClick}>
+        </div>
+    }
+}
+
+class ColorButton extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return <button
+            className="color-button"
+            style={{ backgroundColor: this.props.color }}
+            onClick={this.props.onClick}
+        />
+    }
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(<App />)
