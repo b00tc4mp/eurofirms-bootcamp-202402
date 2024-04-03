064c54f77730c1865e0e07c3f0471d35bbf761d8
Ana María Moya <anamml461@gmail.com>
2024-04-03 10:02:56 +0200

    add Pixel-Art con react and component #205

---
+++ staff/ana-maria-moya/shapes/pixel-art/ColorButtons.jsx
@@ -0,0 +1,16 @@
+class ColorButtons extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render(){
+        return <ul className = 'colors'>
+            {this.props.colors.map(color => <li key={color}>
+                <ColorButton
+                color={color}
+                onClick={() => this.props.onColorClick(color)}
+          />
+          </li>)}
+          </ul>   
+    }
+}  
