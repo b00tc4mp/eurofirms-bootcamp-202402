7b36de73c5bb12190e045ecc6ed22adb36522d9b
Javier Pulido <jpdgranada@gmail.com>
2024-04-03 17:26:52 +0200

    change class components to functions components in pixel-art-function #198

---
+++ staff/javier-pulido/shapes/pixel-art-function/ColorButton.jsx
@@ -0,0 +1,16 @@
+function ColorButton(props) {
+    function handleClick() {
+        console.debug('ColorButton handleClick')
+
+        props.onClick()
+    }
+
+    console.debug('ColorBurron render')
+
+    return <button
+        className={`color-button ${props.selected ? 'color-button-selected' : ''}`}
+        style={{ backgroundColor: props.color }}
+        onClick={() => handleClick()}
+
+    />
+}
