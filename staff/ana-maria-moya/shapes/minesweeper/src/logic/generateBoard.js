a346c9c18f915d7de77a640343aea87c641b39e3
Ana María Moya <anamml461@gmail.com>
2024-04-09 14:33:55 +0200

    add changes in minesweeper #205

---
+++ staff/ana-maria-moya/shapes/minesweeper/src/logic/generateBoard.js
@@ -0,0 +1,55 @@
+function generateBoard(columns, rows, bombs) {
+    const board = new Array(rows)
+
+    const cellsQuantity = columns * rows
+
+    const indexes = []
+
+    for (let i = 0; i < bombs; i++) {
+        const index = Math.floor(Math.random() * cellsQuantity - 1)
+
+        const indexI = Math.floor(index / 8)
+        const indexJ = index % 8
+
+        if (indexes.some((index => index.i === indexI && index.j === indexJ)))
+            i--
+
+        else indexes.push({ i: indexI, j: indexJ })
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        board[i] = new Array(columns);
+
+        for (let j = 0; j < board[i].length; j++) {
+            const isBomb = indexes.some(index => index.j === j && index.i === i)
+
+            const cell = {
+                isBomb,
+                isClicked: false,
+            };
+
+            board[i][j] = cell
+        }
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            let bombsAside = 0
+
+            if (board[i]?.[j - 1]?.isBomb) bombsAside++
+            if (board[i]?.[j + 1]?.isBomb) bombsAside++
+            if (board[i - 1]?.[j]?.isBomb) bombsAside++
+            if (board[i + 1]?.[j]?.isBomb) bombsAside++
+            if (board[i - 1]?.[j - 1]?.isBomb) bombsAside++
+            if (board[i - 1]?.[j + 1]?.isBomb) bombsAside++
+            if (board[i + 1]?.[j - 1]?.isBomb) bombsAside++
+            if (board[i + 1]?.[j + 1]?.isBomb) bombsAside++
+
+            board[i][j].bombsAside = bombsAside
+        }
+    }
+
+    return board
+}
+
+export { generateBoard }
