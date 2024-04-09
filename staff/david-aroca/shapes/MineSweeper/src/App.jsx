d3336def1f0b095548845d58b60d263c0548e703
unknown <davidaroca27@gmail.com>
2024-04-09 14:26:08 +0200

    add logic to the MineSweeper and transcribe code #208

--- staff/david-aroca/shapes/MineSweeper/src/App.jsx
+++ staff/david-aroca/shapes/MineSweeper/src/App.jsx
@@ -1,71 +1,7 @@
+import React from "react"
+import Board from "./Components/Board"
+
 function App() {
-  const columns = 8
-  const rows = 8
-
-  const initialBoard = new Array(rows)
-
-  for (let i = 0; i < initialBoard.length; i++) {
-
-    const isBomb = Math.floor(Math.random() * 8)
-
-    const cell = {
-      isBomb: !isBomb,
-      isClicked: false,
-      bombsAside: 0
-    }
-    initialBoard[i] = new Array(columns).fill({ ...cell })
-  }
-
-  const { board, setBoard } = React.useState(matrix)
-
-  const handleCellClick = (i, j) => {
-    if (gameState === 'loose') return
-
-    const boardCopy = { ...board }.map(row => [...row])
-
-    const cellCopy = { ...boardCopy[i][j] }
-
-    cellCopy.isClicked = true
-
-    boardCopy[i][i] = cellCopy
-
-    setBoard(boardCopy)
-
-    if (cellCopy.isBomb) {
-      setTimeout(() => alert('explotation'), 0)
-
-      setGameState('loose')
-    }
-  }
-
-  const handleResetClick = () => {
-    const newBoard = new Array(rows)
-
-    for (let i = 0; i < newBoard.length; i++) {
-      newBoard[i] = new Array(rows)
-
-      for (let i = 0; i < newBoard[i].length; j++) {
-        newBoard[i][j - 1]
-        newBoard[i][j + 1]
-        newBoard[i - 1][j]
-        newBoard[i - 1][j]
-        newBoard[i][j]
-        newBoard[i][j]
-
-        const isBomb = Math.floor(Math.random() * 10)
-
-        const cell = {
-
-          isBomb: !isBomb,
-          isClicked: false,
-          bombsAside: 0
-        }
-
-      }
-
-    }
-  }
-
   return (
     <>
       <header>
@@ -73,23 +9,11 @@ function App() {
         <hr />
       </header>
       <main className="main">
-        <section className="board"
-          style={{
-            gridTemplateColumns: `repeat(${columns},1fr)`,
-            gridTemplateRows: `repeat(${rows},1fr)`
-          }}>
-          {board.map((row, i) => rows.map((cell, j) => {
-            return <div className="cell">
-              {cell.bombsAside}
-            </div>:
-          <div>
-            <buton onClick={handleCellClick} className='no-clicked-cell'></buton>
-          </div>
-          })).flat()}
-        </section>
-      </main>
+        <Board />
+      </main >
     </>
   )
 }
+
 
 export default App
---
+++ staff/david-aroca/shapes/MineSweeper/src/Components/Board.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { generateBoard } from "../logic/generateBoard"
+import Cell from "./Cell";
+
+const columns = 8
+const rows = 8
+const bombs = 10
+
+const intialBoard = generateBoard(columns, rows, bombs)
+
+function Board() {
+    const [board, setBoard] = useState(intialBoard)
+    const [gameState, setGameState] = useState(null)
+    const [isFlagClicked, setIsFlagClicked] = useState(false)
+
+    const handleCellClick = (i, j) => {
+        if (gameState === 'loose') return
+
+        const boardCopy = [...board].map(row => [...row])
+
+        const cellCopy = { ...boardCopy[i][j] }
+
+        cellCopy.isClicked = true
+
+        boardCopy[i][j] = cellCopy
+
+        setBoard(boardCopy)
+
+        if (cellCopy.isBomb) {
+            setTimeout(() => alert('explotaste'), 20)
+
+            setGameState('loose')
+        }
+    }
+
+    const handleResetClick = () => {
+        const newBoard = generateBoard(columns, rows)
+
+        setBoard(newBoard)
+        setGameState(null)
+    }
+
+    const handleFlagClcik = () => {
+        setBoard(newBoard)
+        setGameState(null)
+    }
+
+    return (
+        <div>
+            <div onClick className="button-buttons">
+
+                <button>Flag</button>
+                <button>Click</button>
+            </div>
+
+            <section
+                className="board"
+                style={{
+                    gridTemplateColumns: `repeat(${columns}, 1fr)`,
+                    gridTemplateRows: `repeat(${rows}, 1fr)`
+                }}
+            >
+                {board.map((row, i) => row.map((cell, j) => (
+                    <Cell
+                        onCellClick={handleCellClick}
+                        coords={{ i, j }}
+                        key={`${i}-${j}`}
+                        cell={cell} />)
+                )).flat()}
+            </section>
+            {gameState === 'loose' && <button onClick={handleResetClick}>Reset</button>}
+        </div>
+    )
+}
+
+export default Board
