97f52acefc733f84f8775cb08d0e5228d4086acc
Javier Moneo <jvr.moneo@gmail.com>
2024-04-09 10:34:01 +0200

    implements mine-sweeper; #201

---
+++ staff/javier-moneo/shapes/mine-sweeper/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Hola Buscaminas</h1>
+        <hr />
+      </header>
+      <main className="main">
+        <Board />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ staff/javier-moneo/shapes/mine-sweeper/src/components/Board.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { generateBoard } from '../logic/generateBoard';
+import Cell from './Cell';
+
+const columns = 8;
+const rows = 8;
+const bombs = 10;
+
+const initialBoard = generateBoard(columns, rows, bombs);
+
+function Board() {
+  const [board, setBoard] = useState(initialBoard);
+  const [gameState, setGameState] = useState(null);
+
+  const handleCellClick = (i, j) => {
+    if (gameState === 'loose') return;
+
+    const boardCopy = [...board].map((row) => [...row]);
+
+    const cellCopy = { ...boardCopy[i][j] };
+
+    cellCopy.isClicked = true;
+
+    boardCopy[i][j] = cellCopy;
+
+    setBoard(boardCopy);
+
+    if (cellCopy.isBomb) {
+      setTimeout(() => alert('explotaste'), 20);
+
+      setGameState('loose');
+    }
+  };
+
+  const handleResetClick = () => {
+    const newBoard = generateBoard(columns, rows, bombs);
+
+    setBoard(newBoard);
+    setGameState(null);
+  };
+
+  return (
+    <div>
+      <section
+        className="board"
+        style={{
+          gridTemplateColumns: `repeat(${columns}, 1fr)`,
+          gridTemplateRows: `repeat(${rows}, 1fr)`,
+        }}
+      >
+        {board
+          .map((row, i) =>
+            row.map((cell, j) => (
+              <Cell
+                onCellClick={handleCellClick}
+                coords={{ i, j }}
+                key={`${i}-${j}`}
+                cell={cell}
+              />
+            ))
+          )
+          .flat()}
+      </section>
+      {gameState === 'loose' && (
+        <button onClick={handleResetClick}>Reset</button>
+      )}
+    </div>
+  );
+}
+
+export default Board;
