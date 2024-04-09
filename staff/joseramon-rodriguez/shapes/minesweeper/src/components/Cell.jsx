644912df13531e7250ca32fa07e22fa912a21d56
jose ramon rodriguez <jrjosera1@gmail.com>
2024-04-09 14:05:46 +0200

    minesweeper is now split into components #200

---
+++ staff/joseramon-rodriguez/shapes/minesweeper/src/components/Cell.jsx
@@ -0,0 +1,20 @@
+function Cell(props) {
+    const { cell, coords, onCellClick } = props
+
+    if (cell.isClicked) {
+        return (
+            <div className="clicked-cell">
+                {cell.isBomb ? 'Boom' : cell.bombsAside}
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="no-clicked-cell">
+                <button onClick={() => onCellClick(coords.i, coords.j)} className="cell-button" />
+            </div>
+        )
+    }
+}
+
+export default Cell
