633093e75f136f97beae9a52357989f3857b9a74
Bernat Alcudia <bernatalcudia@gmail.com>
2024-04-10 18:09:40 +0200

    add functions logic server #60

--- staff/bernat-alcudia/liveshare/api/index.js
+++ staff/bernat-alcudia/liveshare/api/index.js
@@ -14,3 +14,60 @@ client.connect()
             .then(result => console.log(result))
             .catch(error => console.error(error))
     })
+
+
+function registerUser(name, birthdate, email, username, password, callback) {
+    users.findOne({ $or: [{ email }, { username }] })
+        .then(user => {
+            if (user) {
+                callback(new Error('user already exits'))
+                return
+            }
+            user = { name, birthdate, email, username, password }
+
+            users.insertOne(user)
+                .then(() => callback(null))
+                .catch(error => callback(error))
+        })
+        .catch(error => callback(error))
+}
+
+registerUser('Pepito Grillo', '2000-01-01', 'pepito@grillo.com', 'pepitogrillo', '123123123', error => {
+    if (error) {
+        console.error(error)
+
+        return
+    }
+
+    console.log('user registered')
+})
+
+function loginUser(username, password, callback) {
+    users.findOne({ username, password })
+        .then((user) => {
+            if (!user) {
+                callback(new Error('no valid credentials'))
+
+
+                return;
+            }
+            callback(null, user._id.toString());
+        })
+        .catch((error) => console.error(error))
+}
+
+function retrieveUser(userId, callback) {
+    users.findOne({ _id: new ObjectId(userId) })
+        .then((user) => {
+            if (!user) {
+                callback(new Error('user not found'))
+
+                return
+            }
+            delete user._id
+            delete user.password
+            delete user.email
+
+            callback(null, user)
+        })
+}
