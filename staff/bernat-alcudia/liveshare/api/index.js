5bcaf0863771e0109bf9ceed07113fe3cb95d8fb
Bernat Alcudia <bernatalcudia@gmail.com>
2024-04-11 18:29:23 +0200

    add function retrievePosts #60

--- staff/bernat-alcudia/liveshare/api/index.js
+++ staff/bernat-alcudia/liveshare/api/index.js
@@ -41,7 +41,7 @@ registerUser('Pepito Grillo', '2000-01-01', 'pepito@grillo.com', 'pepitogrillo',
 
     console.log('user registered')
 })
-
+// ----------------------------------------------------------------------------------------------------------------
 function loginUser(username, password, callback) {
     users.findOne({ username, password })
         .then((user) => {
@@ -56,6 +56,8 @@ function loginUser(username, password, callback) {
         .catch((error) => console.error(error))
 }
 
+
+// ---------------------------------------------------------------------------------------------------------------------
 function retrieveUser(userId, callback) {
     users.findOne({ _id: new ObjectId(userId) })
         .then((user) => {
@@ -71,3 +73,66 @@ function retrieveUser(userId, callback) {
             callback(null, user)
         })
 }
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+function retrievePosts(userId, callback) {
+    users.findOne({ _id: new ObjectId(userId) })
+        .then(user => {
+            if (!user) {
+                callback(new Error('user not found'))
+
+
+                return
+            }
+
+            let errorHappened = false
+            let postsProcessedCount = 0
+
+            posts.find({}).toArray()
+                .then(posts => {
+                    posts.forEach(post => {
+                        users.findOne({ _id: post.author }, { projection: { username: 1 } })
+                            .then(user => {
+                                if (errorHappened) {
+                                    return
+                                }
+
+                                if (!user) {
+                                    callback(new Error('owner user not found'))
+
+                                    errorHappened = true
+
+                                    return
+                                }
+
+                                post.id = post._id.toString()
+
+                                delete post.id
+
+                                const author = {
+                                    id: post.author.toString(),
+                                    username: user.username
+                                }
+
+                                post.author = author
+
+                                postsProcessedCount++
+
+                                if (postsProcessedCount === posts.length) {
+                                    callback(null, posts)
+                                }
+
+
+                            })
+                            .catch(error => callback(error))
+                    })
+                })
+                .catch(error => callback(error))
+        })
+        .catch(error => callback(error))
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------
