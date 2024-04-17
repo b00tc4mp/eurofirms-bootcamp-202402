3f0571c453f651e01892294f8149972a9ca07ec4
sara-nevado <sarihds@hotmail.com>
2024-04-17 07:24:10 +0200

    new file index.js in folder logic #96

---
+++ staff/sara-nevado/liveshare/api/logic/index.js
@@ -0,0 +1,13 @@
+import createPost from './createPost.js'
+import authenticateUser from './authenticateUser.js'
+
+
+
+
+
+const logic = {
+    authenticateUser,
+    createPost
+}
+
+export default logic
