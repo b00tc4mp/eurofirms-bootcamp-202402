219dbce13b76703197a645fbd1d999daddee4613
alalluna <terealbenca@gmail.com>
2024-06-10 11:53:11 +0200

    last changes for deploying app #247

--- staff/tere-albenca/project/api/logic/updateLesson.js
+++ staff/tere-albenca/project/api/logic/updateLesson.js
@@ -19,25 +19,25 @@ function updateLesson(userId, lessonId, title, image, description, link, video) 
                 throw new MatchError('user not found')
 
             return Lesson.findById(lessonId)
+                .then(lesson => {
+                    if (!lesson)
+                        throw new MatchError('lesson not found')
+
+                    if (userId !== lesson.teacher.toString())
+                        throw new MatchError('you can not edit this lesson')
+
+                    lesson.title = title
+                    lesson.image = image
+                    lesson.description = description
+                    lesson.link = link
+                    lesson.video = video
+
+                    return lesson.save()
+                        .catch(error => { throw new SystemError(error.message) })
+                })
                 .catch(error => { throw new SystemError(error.message) })
+                .then(() => { })
         })
-        .then(lesson => {
-            if (!lesson)
-                throw new MatchError('lesson not found')
-
-            if (lesson.teacher.toString() !== userId)
-                throw new MatchError('you can not edit this lesson')
-
-            lesson.title = title
-            lesson.image = image
-            lesson.description = description
-            lesson.link = link
-            lesson.video = video
-
-            return lesson.save()
-                .catch(error => { throw new SystemError(error.message) })
-        })
-        .then(result => { })
 }
 
 export default updateLesson
--- staff/tere-albenca/project/api/test/update-lesson.sh
+++ staff/tere-albenca/project/api/test/update-lesson.sh
@@ -1,1 +1,6 @@
-curl -X PATCH -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjNiM2ZkYTIyMzJmNjI3NjMxZTExMGYiLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTcxNzY3OTE5OSwiZXhwIjoxNzE3NjgyNzk5fQ.l2sq0OjjYswiAI_eBNwxy45hlOap-pyALt4My-FfDhI' -H 'Content-type: application/json' -d '{"title":"exercise four. Estructure of things","image": "https://cdn.goconqr.com/uploads/media/image/10129500/image_primary_ef50c485-b683-407a-a665-3d0ca428d896.jpg", "description": "En la sesión de hoy hablaremos sobre el encaje, para ello tienes a disposición distintos recursos", "link": "https://prezi.com/view/xo9vedpOpYYxyaGP0l21/"}' http://localhost:9050/lessons/6661a4f4800dad6e2be6c0ef -v
+
+
+curl -X PATCH -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjNiM2ZkYTIyMzJmNjI3NjMxZTExMGYiLCJyb2xlIjoidGVhY2hlciIsImlhdCI6MTcxNzg0NDEyMywiZXhwIjoxNzE3ODQ3NzIzfQ.MRb2LW3vyghgW4dHO8B8fhXAAiXwGJ_jku40KHr48m8' \
+-H 'Content-Type: application/json' \
+-d '{"title": "Exercise four", "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg0LgvYhNnRHtoadYxvakXsj4VR_3gWnISLv4GJ9jbXxi-xxDNnHS2mNmaK9C8hQBhLMam_dSHtVZqz2TjAPbzvEQLKUPo05L-PZT2pnaoyk-DKQAtjowi2-_lqc9reROA_rAcLyxPX8w/s1600/complejas-man-maier01.jpg", "description": "En la sesión de hoy hablaremos sobre el encaje", "link": "https://prezi.com/view/xo9vedpOpYYxyaGP0l21/", "video": "https://www.youtube.com/watch?v=iFQZcZPH7u4&t=23s"}' \
+http://localhost:9050/lessons/6661a4f4800dad6e2be6c0ef -v
